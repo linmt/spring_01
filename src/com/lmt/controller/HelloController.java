379c619edfe65232749bb8b150031d2d3c798553
linmt <43060283+linmt@users.noreply.github.com>
2018-10-23 17:27:53 +0800

    181023

---
+++ src/com/lmt/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.lmt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by 张洲徽 on 2018/10/23.
+ */
+//@org.springframework.stereotype.Controller
+public class HelloController implements Controller{
+    //处理请求的方法
+    @RequestMapping("/hello.form")
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res)
+            throws Exception {
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("hello");//找到hello.jsp
+        mav.getModel().put("message", "模型数据");//传数据，key-value
+        //等价于request.setAttribute("msg", "模型数据");
+        return mav;
+    }
+}
